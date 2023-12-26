b2862ce7010c487b6fdf246856c54ec69773319c
Steven Emokor <emokores256@gmail.com>
2023-12-26 21:05:12 +0300

    fixed silent commands

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,16 +1,18 @@
 return {
-  'akinsho/bufferline.nvim',
-  version = "*",
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function ()
-    vim.keymap.set({'n', 'v'}, '<leader>x', ":bd<CR>")
-    vim.keymap.set('n', '<tab>', ":BufferLineCycleNext<CR>")
-    vim.keymap.set('n', '<S-tab>', ":BufferLineCyclePrev<CR>")
-    vim.opt.termguicolors = true
-    require("bufferline").setup({
-      options = {
-        diagnostics = "nvim-lspconfig"
-      }
-    })
-  end
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local opts = { silent = true, noremap = true }
+
+		vim.keymap.set({ "n", "v" }, "<leader>x", ":bd<CR>", opts)
+		vim.keymap.set("n", "<tab>", ":BufferLineCycleNext<CR>", opts)
+		vim.keymap.set("n", "<S-tab>", ":BufferLineCyclePrev<CR>", opts)
+		vim.opt.termguicolors = true
+		require("bufferline").setup({
+			options = {
+				diagnostics = "nvim-lspconfig",
+			},
+		})
+	end,
 }
