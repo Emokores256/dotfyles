1cdf87ac47fb75812e699cbd83bcd7e0cd3a2636
Steven Emokor <emokores256@gmail.com>
2023-12-26 16:45:34 +0300

    edited configuration for autocompletion

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,55 +1,49 @@
- return {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      'hrsh7th/nvim-cmp',
-      'hrsh7th/cmp-nvim-lsp',
-      'saadparwaiz1/cmp_luasnip',
-      'rafamadriz/friendly-snippets',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'L3MON4D3/LuaSnip',
-      'hrsh7th/cmp-buffer',
-    },
-    config = function ()
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-nvim-lsp",
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"L3MON4D3/LuaSnip",
+		"hrsh7th/cmp-buffer",
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
 
-      vim.opt.completeopt = "menu,menuone,noselect"
+		require("luasnip.loaders.from_vscode")
 
-      cmp.setup({
-        snippet = {
-          expand = function (args)
-            luasnip.expand(args.body)
-          end
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-          ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-          ["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4), -- scroll backward
-          ["<C-f>"] = cmp.mapping.scroll_docs(4), -- scroll forward
-          ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-          ["<C-e>"] = cmp.mapping.abort(), -- clear completion window
-          ["<CR>"] = cmp.mapping.confirm({ select = false }), -- confirm selection
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lspconfig" }, -- lsp 
-          { name = "luasnip" }, -- snippets
-          { name = "buffer" }, -- text within current buffer
-          { name = "path" }, -- file system paths
-        }),
-      })
-    end
-  },
+		cmp.setup({
+			completion = {
+				completeopt = "menu, menuone, preview, noselect",
+			},
 
-  {
-    'L3MON4D3/LuaSnip', dependencies = { "rafamadriz/friendly-snippets" }, build = "make install_jsregexp",
-    config = function ()
-      require('luasnip').setup({
-         require("luasnip.loaders.from_vscode").lazy_load()
-       })
-    end
-  }
-
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+				["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<C-b>"] = cmp.mapping.scroll_docs(-4), -- scroll backward
+				["<C-f>"] = cmp.mapping.scroll_docs(4), -- scroll forward
+				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+				["<C-e>"] = cmp.mapping.abort(), -- clear completion window
+				["<CR>"] = cmp.mapping.confirm({ select = false }), -- confirm selection
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" }, -- lsp
+				{ name = "luasnip" }, -- snippets
+				{ name = "buffer" }, -- text within current buffer
+				{ name = "path" }, -- file system paths
+			}),
+		})
+	end,
+}
