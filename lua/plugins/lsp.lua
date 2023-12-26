4566885d29b85789c093c1034a3824fc1f445fc1
Steven Emokor <emokores256@gmail.com>
2023-12-26 16:43:56 +0300

    improvements in Mason UI

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,26 +1,117 @@
 return {
 	{
 		"williamboman/mason.nvim",
+		dependencies = {
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
 		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"prettier", -- prettier formatter
+					"stylua", -- lua formatter
+					"eslint_d", -- js linter
+					"htmlhint", -- html linter
+					"php-cs-fixer", -- php formatter
+					"phpcs", -- php linter
+					"cspell", -- code speller (linter)
+					"blade-formatter", -- formatter for blade files
+					"js-debug-adapter", -- debugging for javsacript
+					"php-debug-adapter", -- debugging for php
+				},
+			})
+
 			require("mason-lspconfig").setup({
 				ensure_installed = { "tsserver", "lua_ls", "intelephense", "html", "cssls", "tailwindcss", "emmet_ls" },
+				automatic_installation = true,
 			})
 		end,
 	},
 	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function() end,
+	},
+	{
 		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			{ "antosha417/nvim-lsp-file-operations", config = true },
+		},
 		config = function()
-			local on_attach = require("lua.plugins.lsp").on_attach
-			local capabilities = require("lua.plugins.lsp").capabilities
+			-- import cmp-nvim-lsp plugin
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+			local keymap = vim.keymap -- for conciseness
+
+			local opts = { noremap = true, silent = true }
+			local on_attach = function(client, bufnr)
+				opts.buffer = bufnr
+
+				-- set keybinds
+				opts.desc = "Show LSP references"
+				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+
+				opts.desc = "Go to declaration"
+				keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+
+				opts.desc = "Show LSP definitions"
+				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+
+				opts.desc = "Show LSP implementations"
+				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+
+				opts.desc = "Show LSP type definitions"
+				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+
+				opts.desc = "See available code actions"
+				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+				opts.desc = "Smart rename"
+				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+
+				opts.desc = "Show buffer diagnostics"
+				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+				opts.desc = "Show line diagnostics"
+				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+				opts.desc = "Go to previous diagnostic"
+				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+				opts.desc = "Go to next diagnostic"
+				keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+				opts.desc = "Show documentation for what is under cursor"
+				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+				opts.desc = "Restart LSP"
+				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+			end
+
+			-- used to enable autocompletion (assign to every lsp server config)
+			local capabilities = cmp_nvim_lsp.default_capabilities()
+
+			-- Change the Diagnostic symbols in the sign column (gutter)
+			local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			end
+
+			local lspconfig = require("lspconfig")
 
 			local servers = { "html", "cssls", "intelephense", "emmet_ls", "tailwindcss", "tsserver" }
-			local lspconfig = require("lspconfig")
 
 			for _, lsp in ipairs(servers) do
 				lspconfig[lsp].setup({
@@ -29,21 +120,45 @@ return {
 				})
 			end
 
-			lspconfig.lua_ls.setup({})
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						-- make the language server recognize "vim" global
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							-- make language server aware of runtime files
+							library = {
+								[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+								[vim.fn.stdpath("config") .. "/lua"] = true,
+							},
+						},
+					},
+				},
+			})
+
 			lspconfig.tsserver.setup({})
+
 			lspconfig.cssls.setup({})
+
 			lspconfig.tailwindcss.setup({})
-			lspconfig.emmet_ls.setup({})
+
+			lspconfig.emmet_ls.setup({
+				filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "blade" },
+			})
+
 			lspconfig.html.setup({})
+
 			lspconfig.intelephense.setup({
 				init_options = {
 					globalStoragePath = "/usr/bin/intelephense",
 					licenseKey = "~/intelephense/license.txt",
 				},
 			})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			--[[ vim.keymap.set("n", "K", vim.lsp.buf.hover, {}) ]]
+			--[[ vim.keymap.set("n", "gd", vim.lsp.buf.definition, {}) ]]
+			--[[ vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {}) ]]
 		end,
 	},
 }
