e9bf1a1495524be75db863d99e3f57f8776cd9bc
Steven Emokor <emokores256@gmail.com>
2023-12-26 17:51:11 +0300

    modified plugin settings

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,6 +9,31 @@ return {
 	},
 
 	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+
+		--keymaps
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal float<CR>")
+		vim.keymap.set("n", "<S-n>", ":Neotree float git_status<CR>")
+
+		require("neo-tree").setup({
+			git_status = {
+				symbols = {
+					-- Change type
+					added = "✚", -- or "✚", but this is redundant info if you use git_status_colors on the name
+					modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+					deleted = "✖", -- this can only be used in the git_status source
+					renamed = "󰁕", -- this can only be used in the git_status source
+					-- Status type
+					untracked = "",
+					ignored = "",
+					unstaged = "󰄱",
+					staged = "",
+					conflict = "",
+				},
+			},
+		})
 	end,
 }
