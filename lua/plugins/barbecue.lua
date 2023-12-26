9b3376d7b2efb0d159d1b2d11cdea44560f5c6a9
Steven Emokor <emokores256@gmail.com>
2023-12-26 21:04:56 +0300

    fixed barbecue not working

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -1,12 +1,16 @@
 return {
-  "utilyre/barbecue.nvim",
-  name = "barbecue",
-  version = "*",
-  dependencies = {
-    "SmiteshP/nvim-navic",
-    "nvim-tree/nvim-web-devicons", -- optional dependency
-  },
-  config = function ()
-    require('barbecue.ui').toggle(true)
-  end
+	"utilyre/barbecue.nvim",
+	name = "barbecue",
+	lazy = false,
+	version = "*",
+	dependencies = {
+		"SmiteshP/nvim-navic",
+		"nvim-tree/nvim-web-devicons", -- optional dependency
+	},
+	config = function()
+		require("barbecue").setup({
+			symbols = {},
+			kinds = {},
+		})
+	end,
 }
