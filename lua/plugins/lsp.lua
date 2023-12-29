e3da28fb8e4d0a7db457102b59c579df5afed2f8
Steven Emokor <emokores256@gmail.com>
2023-12-29 19:10:04 +0300

    fixed keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -25,7 +25,7 @@ return {
 					"eslint_d", -- js linter
 					"htmlhint", -- html linter
 					"pint", -- php formatter for laravel/php
-          -- "php-cs-fixer", -- php formatter
+					-- "php-cs-fixer", -- php formatter
 					-- "phpcs", -- php linter
 					"markdownlint", -- markdown linter
 					"cspell", -- code speller (linter)
@@ -87,26 +87,26 @@ return {
 				opts.desc = "Show LSP type definitions"
 				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-				opts.desc = "See available code actions"
-				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+				--[[ opts.desc = "See available code actions" ]]
+				--[[ keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection ]]
 
-				opts.desc = "Smart rename"
-				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+				--[[ opts.desc = "Smart rename" ]]
+				--[[ keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename ]]
 
-				opts.desc = "Show buffer diagnostics"
-				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+				--[[ opts.desc = "Show buffer diagnostics" ]]
+				--[[ keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file ]]
 
-				opts.desc = "Show line diagnostics"
-				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+				--[[ opts.desc = "Show line diagnostics" ]]
+				--[[ keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line ]]
 
-				opts.desc = "Go to previous diagnostic"
-				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+				--[[ opts.desc = "Go to previous diagnostic" ]]
+				--[[ keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer ]]
 
-				opts.desc = "Go to next diagnostic"
-				keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+				--[[ opts.desc = "Go to next diagnostic" ]]
+				--[[ keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer ]]
 
-				opts.desc = "Show documentation for what is under cursor"
-				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+				--[[ opts.desc = "Show documentation for what is under cursor" ]]
+				--[[ keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor ]]
 
 				opts.desc = "Restart LSP"
 				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
@@ -171,9 +171,15 @@ return {
 					licenseKey = "~/intelephense/license.txt",
 				},
 			})
-			--[[ vim.keymap.set("n", "K", vim.lsp.buf.hover, {}) ]]
-			--[[ vim.keymap.set("n", "gd", vim.lsp.buf.definition, {}) ]]
-			--[[ vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {}) ]]
+			vim.keymap.set(
+				"n",
+				"K",
+				vim.lsp.buf.hover,
+				{ desc = "Shows the documentation of what is under the cursor" }
+			)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Shows definition" }, opts)
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" }, opts)
+			vim.keymap.set({ "n", "v" }, "<leader>l", vim.diagnostic.open_float, { desc = "Show line diagnostic" }, opts)
 		end,
 	},
 }
